d4bd9f4f2ae6c3aab751209cd8ed4494b623ab02
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-03-15 18:43:47 -0500

    finished stingifyId section

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -16,8 +16,32 @@
  * ]) === 'ABC, abc, CBA'
  */
 
-const stringifyId = (arr) => {
+// const stringifyId = (arr) => {
+//     const idArray = arr.map(obj => obj.id)
+//     const uniqueArray = []
+//     idString = ""
 
-};
+//     for (let id of idArray) {
+//         if (!uniqueArray.includes(id)) {
+//             uniqueArray.push(id)
+//         }
+//     }
+
+//     for (let i = 0; i < uniqueArray.length; ++i) {
+//         if(i !== uniqueArray.length - 1) {
+//             idString += `${uniqueArray[i]}, `
+//         }
+//         else {
+//             idString += `${uniqueArray[i]}`
+//         }
+//     }
+//     return idString
+// };
+
+const stringifyId = (arr) => {
+    const idArray = arr.map(obj => obj.id);
+    const uniqueArray = [...new Set(idArray)]
+    return uniqueArray.join(", ")
+}
 
 module.exports = stringifyId;
