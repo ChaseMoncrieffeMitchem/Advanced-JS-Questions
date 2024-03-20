551f88588ba4534bd9c812db32d88de6f4683ed3
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-03-19 19:48:53 -0500

    js questions

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -9,27 +9,44 @@
 const maxCharacter = (str) => {
   // creat a character map
 
-  charMap = {}
-  let max = 0
-  let maxChar = ""
+//   charMap = {}
+//   let max = 0
+//   let maxChar = ""
 
-  for (let char of str) {
-    charMap[char] = charMap[char] + 1 || 1
-  }
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
 
-  // Loop over every character in the character map
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-        max = charMap[char]
+//   // Loop over every character in the character map
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//         max = charMap[char]
+//         maxChar = char
+//     }
+//     return maxChar
+//   }
+//     // If the value of the character is greater than max
+//       // set max to the value
+//       // set maxChar to the character
+
+//   // Return maxChar
+
+const charMap = new Map();
+let max = 0
+let maxChar = ""
+
+for (const char of str) {
+    charMap.set(char, charMap.get(char) + 1 || 1)
+}
+
+for (const [char, count] of charMap) {
+    if (count > max) {
+        max = count
         maxChar = char
     }
-    return maxChar
-  }
-    // If the value of the character is greater than max
-      // set max to the value
-      // set maxChar to the character
+}
 
-  // Return maxChar
+return maxChar;
 };
 
 module.exports = maxCharacter;
