51774ef5a7817b2100838c19e0b55f837adb3f4d
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-04-01 17:26:16 -0500

    finished searchRotatedArray

---
+++ DSA Questions/searchRotatedArray/index.js
@@ -0,0 +1,45 @@
+/** Search in Rotated Sorted Array **
+ * 
+ * Given a rotated sorted array and a target, 
+ * return the index of the number in the array 
+ * which equals to the target.
+ * 
+ * Note: Your solutions' time complexity must be faster than O(n). 
+ * 
+ * @example
+ * search([5, 4, 1, 2, 3], 2) -> 3
+ * search([7, 9, 12, 3, 4], 7) -> 0
+ * search([3, 4, 2, 0], 4) -> 1
+ * 
+ */
+
+const search = (nums, target) => {
+    let l = 0 
+    let r = nums.length - 1
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+
+        if (nums[mid] === target) return mid
+
+        if (nums[mid] < nums[r]) {
+            if (target < nums[mid] || target > nums[r]) {
+                r = mid - 1
+            }
+            else {
+                l = mid + 1
+            }
+        }
+        else {
+            if (target < nums[l] || target > nums[mid]) {
+                l = mid + 1
+            }
+            else {
+                r = mid - 1
+            }
+        }
+    }
+    return "not found"
+}
+
+module.exports = search;
